9be091296faaf1b7f43eb5c48eacd7c1bd3860e5
Ethan Nguyen <etnguyen03@hotmail.com>
2018-07-02 12:02:04 -0400

    Log files

--- CSPCRecallsBot.py
+++ CSPCRecallsBot.py
@@ -32,6 +32,10 @@ secretFile = open("secret.txt").read().split("\n")
 reddit = praw.Reddit(user_agent='CSPCRecallsBot', client_id=secretFile[0], client_secret=secretFile[1], username=secretFile[2], password=secretFile[3])
 subreddit = reddit.subreddit('CPSCRecalls')
 
+logFile = open("logFile.txt", "a")
+
+submissionsList = []
+
 for index in indexList:
     # Check to see if it's already been submitted
     search = list(subreddit.search("url:"+feed['entries'][index]['link']))
@@ -40,3 +44,7 @@ for index in indexList:
         subTitle = re.sub(r'\([^)]*\)', '', subTitle).rstrip()
         submission = subreddit.submit(subTitle, url=feed['entries'][index]['link'])
         submission.reply("> " + str(feed['entries'][index]['summary']) + "\n\n^(This message was posted by a bot. [source](https://github.com/etnguyen03/CPSCRecallsBot/)")
+        submissionsList.append(str(submission))
+
+logFile.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\t\t" + len(submissionsList) + "\t\t" + str(submissionsList))
+logFile.close()
